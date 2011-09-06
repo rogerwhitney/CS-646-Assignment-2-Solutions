7fb8b6a1714cc918e47396335ae923c02fb7c2e0
Roger Whitney <whitney@cs.sdsu.edu>
2011-09-05 19:03:18 -0700

    Initial Commit

---
+++ Assignment2Tests/Assignment2Tests.h
@@ -0,0 +1,13 @@
+//
+//  Assignment2Tests.h
+//  Assignment2Tests
+//
+//  Created by Roger Whitney on 9/5/11.
+//  Copyright (c) 2011 San Diego State University. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Assignment2Tests : SenTestCase
+
+@end
---
+++ Assignment2Tests/Assignment2Tests.m
@@ -0,0 +1,32 @@
+//
+//  Assignment2Tests.m
+//  Assignment2Tests
+//
+//  Created by Roger Whitney on 9/5/11.
+//  Copyright (c) 2011 San Diego State University. All rights reserved.
+//
+
+#import "Assignment2Tests.h"
+
+@implementation Assignment2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Assignment2Tests");
+}
+
+@end
